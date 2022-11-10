0ff21cbde9aa91e6cf17eff28fca85469012e28d
golosalex <golosalex1>
2022-11-10 06:09:33 +0300

    обновлено 1-е задание

--- task1/Program.cs
+++ task1/Program.cs
@@ -17,6 +17,10 @@ internal class Program
         Console.WriteLine(MethodForTask("abc dfg tyu yui"));
 
         Console.WriteLine(MethodForTask("qwertyjnsjnksnk qwertyuiopo qwertyuisnjnsjsnkanmk"));
+        Console.WriteLine("__________________");
+        Console.WriteLine( MethodForTaskV2("a abc abcd abe abk"));
+        Console.WriteLine( MethodForTaskV2("abc abc abcd abe abk helloworld4 helloworld1") ); 
+
     }
 
     static string MethodForTask(string str)
@@ -42,6 +46,54 @@ internal class Program
                 sb.Append(word[i]);
             }
             else break;
+        }
+        return sb.ToString();
+    }
+
+    //Добрый день,
+    //ниже ревью одного из наших программистов
+    //первое неправильное, а третьего нет(если хочет фидбек пусть попробует в первом тесткейсы "a abc abcd abe abk"
+    //(должно быть abc, выдает a) или "abc abc abcd abe abk helloworld4 helloworld1"
+    //(должно быть helloworld, выдает что нет общего начала)
+
+    // предположу что вы имеете ввиду что наибольшее общее начало - это если минимум 2 слова имеют общее начало. 
+    // ниже мое решение, но меня сильно смущает, что претензии не соотносятся с исходным заданием
+
+    private static string MethodForTaskV2(string str)
+    {
+        string result = "-";
+        var words = str.Split(' ').ToList();
+
+        words.Sort();
+        for(int i=0; i<words.Count-1; i++)
+        {
+            string same = CompareTwoWord(words[i], words[i + 1]);
+            if (same.Length >= result.Length)
+            {
+                result = same;
+            }
+        }
+        
+        return result;
+
+    }
+
+    private static string CompareTwoWord(string word1, string word2)
+    {
+        StringBuilder sb = new StringBuilder();
+        int minLength = Math.Min(word1.Length, word2.Length);
+        for (int i = 0; i < minLength; i++)
+        {
+            if (word1[i] == word2[i])
+            {
+                sb.Append(word1[i]);
+            }
+            else
+            {
+                if (sb.Length == 0) return String.Empty;
+                break;
+            }
+            
         }
         return sb.ToString();
     }
